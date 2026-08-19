a93b98e576913b660f9dd72469be08136deff551
manish birthal <manishbirthal4717@gmail.com>
2026-08-20 01:04:08 +0530

    reduce loader time

--- 9exTattooStudio-main/src/App.jsx
+++ 9exTattooStudio-main/src/App.jsx
@@ -30,7 +30,7 @@ export default function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 5000);
+    const timer = setTimeout(() => setLoading(false), 3000);
     return () => clearTimeout(timer);
   }, []);
 
