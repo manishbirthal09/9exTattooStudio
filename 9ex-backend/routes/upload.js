4346ad24bfc1f3f2cd08dc07384dfcd0721d942f
manish birthal <manishbirthal4717@gmail.com>
2026-08-19 18:21:02 +0530

    update upload.js

--- 9ex-backend/routes/upload.js
+++ 9ex-backend/routes/upload.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { getUploadSignature } from '../controllers/uploadController.js';
 // apna existing admin auth middleware import karo yahan
- import { protect } from '../middleware/authMiddleware.js';
+ import { protect } from '../middleware/auth.js';
 
 const router = express.Router();
 
